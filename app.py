73d42737feb9e28b2bf4eff6b8707b25a082ed05
Aditta-das <ndas5662@gmail.com>
2026-04-11 17:22:23 +0600

    testing

--- app.py
+++ app.py
@@ -12,12 +12,12 @@ def home():
 
 
 @app.get("/top_players")
-def top_players(limit: int = 100):
+def get_top_players(limit: int = 100, gender: str = "women/open"):
     try:
         headers = {
             "User-Agent": "Mozilla/5.0"
         }
-        url = "https://ratings.fide.com/a_top.php?list=open"
+        url = f"https://ratings.fide.com/a_top.php?list={gender}"
         response = requests.get(url, headers=headers, timeout=10)
         response.raise_for_status()
         soup = BeautifulSoup(response.text, "html.parser")
@@ -49,48 +49,9 @@ def top_players(limit: int = 100):
             "error": str(e)
         }
 
-@app.get("/top_women_players")
-def top_women_players(limit: int = 100):
-    try:
-        headers = {
-            "User-Agent": "Mozilla/5.0"
-        }
-        url = "https://ratings.fide.com/a_top.php?list=women"
-        response = requests.get(url, headers=headers, timeout=10)
-        response.raise_for_status()
-        soup = BeautifulSoup(response.text, "html.parser")
-        table_selector = ".top_recors_table"
-        table = soup.select_one(table_selector)
-        rows: list = table.find_all("tr")
-        rows.pop(0)
-        top_women_players = []
-        for i, row in enumerate(rows):
-            if i+1 <= limit:
-                raw_row = []
-                for column in row.find_all("td"):
-                    raw_data = column.get_text().replace(u'\xa0', '')
-                    raw_row.append(raw_data)
-                    player_url = column.find("a")
-                    if player_url: 
-                        raw_row.append(player_url["href"].split("/")[-1])
-                top_women_players.append({
-                  "rank": raw_row[0],
-                  "name": raw_row[1],
-                  "fide_id": raw_row[2],
-                  "country": raw_row[3].strip(),
-                  "rating": raw_row[4],
-                })
-        return top_women_players
-
-    except Exception as e:
-        return {
-            "error": str(e)
-        }
-
-
 
 @app.get("/top_country_players")
-def top_country_players(limit: int = 100, country: str = "BAN", gender: str = 'M'):
+def get_top_country_players(limit: int = 100, country: str = "BAN", gender: str = 'M'):
     try:
         headers = {
             "User-Agent": "Mozilla/5.0",
